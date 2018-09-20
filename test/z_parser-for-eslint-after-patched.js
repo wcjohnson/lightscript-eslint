431461b3577900b0e020e4c56593d7ead1da8380
William Johnson <wcjohnson@oigroup.net>
2018-09-19 17:49:13 -0700

    feat: Use @lightscript/transform API

--- test/z_parser-for-eslint-after-patched.js
+++ test/z_parser-for-eslint-after-patched.js
@@ -16,30 +16,4 @@ describe("https://github.com/babel/babel-eslint/issues/558", () => {
     ]);
   });
 
-  /*
-   * This test ensures that the enhanced referencer does not get used if eslint-scope has already been
-   * monkeypatched, because this causes some correctness issues. For example, if the enhanced referencer
-   * is used after the original referencer is monkeypatched, type annotation references are counted twice.
-   */
-  it("does not visit type annotations multiple times after monkeypatching and calling parseForESLint()", () => {
-    assertImplementsAST(
-      espree.parse("foo", { sourceType: "module" }),
-      babelEslint.parse("foo", {})
-    );
-    const parseResult = babelEslint.parseForESLint(
-      "type Foo = {}; function x(): Foo {}",
-      {
-        eslintVisitorKeys: true,
-        eslintScopeManager: true,
-      }
-    );
-    assert(parseResult.visitorKeys);
-    assert(parseResult.scopeManager);
-
-    const fooVariable = parseResult.scopeManager.getDeclaredVariables(
-      parseResult.ast.body[0]
-    )[0];
-
-    assert.strictEqual(fooVariable.references.length, 1);
-  });
 });
