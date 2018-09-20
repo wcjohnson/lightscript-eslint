6a18d8b05cf59083d68e93f9e3f05bd71dfaa2bd
William Johnson <wcjohnson@oigroup.net>
2018-09-19 19:27:10 -0700

    build: Disable prettier linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
   root: true,
   extends: "babel",
-  "plugins": [
-    "prettier"
-  ],
   rules: {
     "no-var": 0,
     "max-len": 0,
-    "prettier/prettier": "error",
   },
   env: {
     node: true,
