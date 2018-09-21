564afc13b26f0747e8b5bad231b13083467047dc
William Johnson <wcjohnson@oigroup.net>
2018-09-21 13:21:50 -0700

    feat(rules): `unnecessaryComma`

---
+++ test/fixtures/lightscript/rules/unnecessaryComma/negative/input.js
@@ -0,0 +1,32 @@
+import {
+  f
+  g
+} from 'somewhere'
+
+func(arg1
+  arg2) -> {
+    arg1~f(arg2
+      g
+    )
+  }
+
+g(func
+  "era"
+  [f
+  g]
+  )
+
+func2(
+  [a
+  b]
+  {c
+  d}
+) ->
+  (a
+    b, c
+    d)
+
+export {
+  func
+  func2
+}
