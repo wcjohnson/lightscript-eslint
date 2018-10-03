8e0005f8c0eb0991c1233f7be0dda25a548f5b56
William Johnson <wcjohnson@oigroup.net>
2018-10-03 02:12:14 -0700

    fix: `no-unused-expressions` works with variants of `CallExpression`s

--- test/fixtures/lightscript/brokenRules/no-unused-expressions/input.js
+++ test/fixtures/lightscript/brokenRules/no-unused-expressions/input.js
@@ -2,3 +2,10 @@ f() ->
   1
 
 f([...for elem e in []: [e]])
+
+x = {}
+x.something?.someFunction()
+x?~something.someFunction()
+x~something?.someFunction()
+x~something.someFunction?()
+x~someFunction()
