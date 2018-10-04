2cfc081ed9adea4b926632360dba52dab342b4de
William Johnson <wcjohnson@oigroup.net>
2018-10-03 20:31:53 -0700

    test: Add more tests for no-unused-expr

--- test/fixtures/lightscript/brokenRules/no-unused-expressions/input.js
+++ test/fixtures/lightscript/brokenRules/no-unused-expressions/input.js
@@ -9,3 +9,21 @@ x?~something.someFunction()
 x~something?.someFunction()
 x~something.someFunction?()
 x~someFunction()
+
+export createPermalink(state, origUrl) ->
+  packed = JSON.stringify(state)
+  // packed = msgpack.encode(state)
+  console.log("permalinking to state", packed, " uncompressed length (bytes) ", packed.length)
+  deflated = pako.deflate(packed, { to: 'string', level: 9 })
+  stateArg = base64url.encode(deflated)
+
+  parsed = url.parse(origUrl)
+  delete parsed.search
+  parsed.query = {
+    v: 2
+    s: stateArg
+  }
+
+  newUrl = url.format(parsed)
+
+  newUrl
