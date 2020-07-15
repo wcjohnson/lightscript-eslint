7a217d3a30551e64d1bfb51aa65195bb487874d3
wcjohnson <wcjohnson@oigroup.net>
2020-07-15 04:24:35 -0700

    fix(linter): fixes for unnecessarySemi rule
    - Don't trigger multiple times for the same semicolon
    - Trigger for `; }`

---
+++ test/fixtures/lightscript/rules/unnecessarySemi/regression/wrong-line-number/input.js
@@ -0,0 +1,10 @@
+getGlobal() ->
+  // eslint-disable-next-line no-undef
+  if (typeof self !== 'undefined') { return self; }
+  // eslint-disable-next-line no-undef
+  if (typeof window !== 'undefined') { return window; }
+  // eslint-disable-next-line no-undef
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+
+getGlobal()
